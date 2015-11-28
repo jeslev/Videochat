13de8483b1115b95391f2ee20d5edc2bbf4be6ad
jeslev4 <jeslev4@gmail.com>
2015-11-28 12:33:40 -0500

    creando proyecto

---
+++ README.md
@@ -0,0 +1,17 @@
+Proyecto Final : VideoChat
+
+*Cliente*
+- En branch principal
+- Se necesita android studio
+- API > 19
+
+*Servidor*
+- En branch 'server'
+- Instalar nodejs
+- Ejecutar servidor ( 'node index.js')
+
+*Instalar nodejs (Ubuntu)*
+- sudo apt-get install nodejs
+- sudo apt-get install npm
+- npm install peer
+- npm install ip
---
+++ app/src/main/java/com/concurrente/testwebrtc/MainActivity.java
@@ -0,0 +1,55 @@
+package com.concurrente.testwebrtc;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.xwalk.core.XWalkPreferences;
+import org.xwalk.core.XWalkView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private XWalkView xWalkWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        xWalkWebView=(XWalkView)findViewById(R.id.xwalkWebView);
+        //xWalkWebView.load("http://192.168.0.12:9000", null);
+
+        // this loads a file from the assets/ directory
+        xWalkWebView.load("file:///android_asset/index.html", null);
+
+        // turn on debugging
+        XWalkPreferences.setValue(XWalkPreferences.REMOTE_DEBUGGING, true);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (xWalkWebView != null) {
+            xWalkWebView.pauseTimers();
+            xWalkWebView.onHide();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (xWalkWebView != null) {
+            xWalkWebView.resumeTimers();
+            xWalkWebView.onShow();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (xWalkWebView != null) {
+            xWalkWebView.onDestroy();
+        }
+    }
+
+}
